2d4d751837bbe79d5408bbd5a1da9e3f8ea941cb
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-23 12:08:54 -0700

    added file uploads for images see settings.py line 132 urls.py line 24 modles.py line 25 post.html line 6 and post-detail.html line 32

--- my_site/urls.py
+++ my_site/urls.py
@@ -15,11 +15,16 @@ Including another URLconf
 """
 
 
+
+
 from django.contrib import admin
 from django.urls import path, include
 
+# allow to display uploads
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     # include urls from the blog app
     path("", include("blog.urls"))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
