95f4031179ad58fb59e5ae34b19fe2f7dcf69b5c
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-23 10:32:33 -0700

    rendering data from models see views.py line 12

--- blog/views.py
+++ blog/views.py
@@ -1,73 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from datetime import date
+from .models import Post
 # Create your views here.
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-all_posts = [
-    {
-        "slug": "iuyoiuyoiuyoi",
-        "image": "chillies.png",
-        "author": "oiuyoiuy",
-        "date": date(2022, 7, 21),
-        "title": "mountian",
-        "excerpt": "iuyoiuyoiuyoiu",
-        "content": 'oiuyoiuyoiuyiouyoiuyioyu'
-    },
-        {
-        "slug": "iuyoiuyoiuyoi",
-        "image": "chillies.png",
-        "author": "oiuyoiuy",
-        "date": date(2022, 8, 21),
-        "title": "opiupouipoiu",
-        "excerpt": "iuyoiuyoiuyoiu",
-        "content": 'oiuyoiuyoiuyiouyoiuyioyu'
-    },
-        {
-        "slug": "iuyoiuyoiuyoi",
-        "image": "chillies.png",
-        "author": "oiuyoiuy",
-        "date": date(2022, 9, 21),
-        "title": "gbvbvbvbvbvbv",
-        "excerpt": "iuyoiuyoiuyoiu",
-        "content": 'oiuyoiuyoiuyiouyoiuyioyu'
-    }
-]
 
 # get the date
 def get_date(post):
     return post['date']
 
 def starting_page(request):
-    # sort by date
-    sorted_posts = sorted(all_posts, key=get_date)
-    # last 3 items
-    latest_posts = sorted_posts[-3:]
+    # filter by date des order
+    latest_posts = Post.objects.all().order_by('-date')[:3]
+    # # sort by date
+    # sorted_posts = sorted(all_posts, key=get_date)
+    # # last 3 items
+    # latest_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {"posts": latest_posts})
 
 def posts(request):
+    all_posts = Post.objects.all().order_by('-date')
     return render(request, "blog/all-posts.html", {"all_posts": all_posts})
 
 def post_detail(request, slug):
     # find slug for post
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
     return render(request, "blog/post-detail.html", {"post": identified_post})
