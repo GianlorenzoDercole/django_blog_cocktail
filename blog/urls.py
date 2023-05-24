9ecb4fc7762a1241f0b6b443f5315d5af73a5237
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-24 09:53:16 -0700

    added class based views see urls.py line 8 and views.py line 23

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,12 @@ from . import views
 
 urlpatterns = [
     # home page
-    path("", views.starting_page, name="starting-page"),
+    # path("", views.starting_page, name="starting-page"),
+    path("", views.StartingPageView.as_view(), name="starting-page"),
     # posts page
-    path("posts", views.posts, name="posts-page"),
+    # path("posts", views.posts, name="posts-page"),
+    path("posts", views.AllPostsView.as_view(), name="posts-page"),
     # post page - slug is unique identifier
-    path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    # path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
     ]
