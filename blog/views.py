bb5ee958ea323da7fd37ae1f6115de450a3fb86d
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-25 16:32:45 -0700

    displaying comments see views.py line 61 and post-deatil.html line 35

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 
+from django.http import HttpResponseRedirect
 from django.views.generic import ListView, DetailView
+from django.urls import reverse
 from .models import Post
+from django.views import View
 from .forms import CommentForm
 # Create your views here.
 
@@ -46,15 +49,44 @@ class AllPostsView(ListView):
 
 
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data( **kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().order_by("-id")
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+
+        post = Post.objects.get(slug=slug)
+
+        if comment_form.is_valid():
+            # don't save to data base
+            comment = comment_form.save(commit=False)
+            # attach comment to post
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+
+
+
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm(),
+            "comments": post.comments.all().oder_by("-id")
+        }
+
+        return render(request, "blog/post-detail.html", context )
+
+
 
 
 # def post_detail(request, slug):
