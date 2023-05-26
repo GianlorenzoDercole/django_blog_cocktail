316132b2096f11e4f2cebc759384c16fb445f1d9
GianlorenzoDercole <johndercole01@gmail.com>
2023-05-26 14:37:35 -0700

    added read later button see views.py line 92 urls.py line 14 post-detail.html line 23 and stored-posts.html line 14

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
 from . import views
-
 urlpatterns = [
     # home page
     # path("", views.starting_page, name="starting-page"),
@@ -11,5 +10,6 @@ urlpatterns = [
     path("posts", views.AllPostsView.as_view(), name="posts-page"),
     # post page - slug is unique identifier
     # path("posts/<slug:slug>", views.post_detail, name="post-detail-page")
-    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page")
+    path("posts/<slug:slug>", views.SinglePostView.as_view(), name="post-detail-page"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
     ]
